6717e51d18617a7f10cf0981d9d0122602fc0475
just X // Developer OxyHub <76210400+OxyHub-Team@users.noreply.github.com>
2024-10-19 17:45:22 +0300

    Update orion.lua

--- orion.lua
+++ orion.lua
@@ -14,12 +14,12 @@ local OrionLib = {
 	Flags = {},
 	Themes = {
 		Default = {
-			Main = Col or3.fromRGB(0, 0, 0),
-			Second = Color3.fromRGB(0, 0, 0),
+			Main = Color3.fromRGB(25, 25, 25),
+			Second = Color3.fromRGB(32, 32, 32),
 			Stroke = Color3.fromRGB(60, 60, 60),
 			Divider = Color3.fromRGB(60, 60, 60),
-			Text = Color3.fromRGB(135, 206, 250),
-			TextDark = Color3.fromRGB(135, 206, 250)
+			Text = Color3.fromRGB(240, 240, 240),
+			TextDark = Color3.fromRGB(150, 150, 150)
 		}
 	},
 	SelectedTheme = "Default",
